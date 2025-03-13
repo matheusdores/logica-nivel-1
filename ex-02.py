5616bdd5f47851ee2e92b8680cd0b5489d0c7d90
Matheus Dores <105971237+matheusdores@users.noreply.github.com>
2025-03-13 19:17:21 -0300

    Add files via upload

---
+++ ex-02.py
@@ -0,0 +1,7 @@
+nome = input("Qual o seu nome:")
+estado = input("Qual estado voce mora:")
+municipio = input("Qual o seu municipio:")
+
+print("Seu nome:", nome)
+print("O estado que voçê mora é:", estado)
+print("Seu municipio:", municipio)
