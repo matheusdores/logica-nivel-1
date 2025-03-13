82ada67dc60c981066935f98cc4d0d479bd65f9a
Matheus Dores <105971237+matheusdores@users.noreply.github.com>
2025-03-13 19:59:16 -0300

    Add files via upload

---
+++ ex-05.py
@@ -0,0 +1,12 @@
+aline = int(9.0)
+mario = int(10)
+segio = int(4.5)
+shirley = int(7.0)
+
+print("ALUNO(A)          NOTA")
+print("=========         =====")
+print("ALINE              ",aline)
+print("MARIO              DEZ")
+print("SERGIO             ",segio)
+print("SHIRLEY            ",shirley)
+
---
+++ ex-06.py
@@ -0,0 +1,6 @@
+print("0 - Fim")
+print("1- Inclui")
+print("2 - Altera")
+print("3 - Exclui")
+print("4 - Consulta")
+print("Opção:")
